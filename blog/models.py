1d4843b95830d619df6cdc4c70016cc4177f0400
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2019-07-15 10:36:19 -0400

    Restart blog app

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class Blog(models.Model):
-    image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+class 
